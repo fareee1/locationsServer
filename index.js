834f4b32998f09107e9849230dbf006cf5ca2ebf
faris <faris.bilalagic17@gmail.com>
2018-05-15 20:18:06 +0200

    locations added

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 const express= require('express')
-const pg = require('pg')
-const { Client } = require('pg');
 const app = express()
 const bodyParser = require('body-parser')
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
-  });
-  
 
 // Allow client to access cross domain or ip-address
 app.use((req,res,next)=>{
@@ -17,25 +10,61 @@ app.use((req,res,next)=>{
     next();
 })
 
-client.connect()
+
+const locations = [{
+    "id": 1,
+    "lat": 52.3702,
+    "lon": 4.8952,
+    "cityName": "Amsterdam",
+    "description": "Such a lovely place to visit",
+    "visited": true
+},{
+    "id": 2,
+    "lat": 50.1109,
+    "lon": 8.6821,
+    "cityName": "Frankfurt",
+    "description": "Visited twice",
+    "visited": true
+},{
+    "id": 3,
+    "lat": 50.0755,
+    "lon": 14.4378,
+    "cityName": "Prague",
+    "description": "Feel in love with this city",
+    "visited": true
+},{
+    "id": 4,
+    "lat": 40.4168,
+    "lon": 3.7038,
+    "cityName": "Madrid",
+    "description": "Planning to visit in october",
+    "visited": false
+},{
+    "id": 5,
+    "lat": 44.7866,
+    "lon": 20.4489,
+    "cityName": "Belgrade",
+    "description": "Best nightlife",
+    "visited": false
+},{
+    "id": 6,
+    "lat": 48.8566,
+    "lon": 2.3522,
+    "cityName": "Paris",
+    "description": "City of love",
+    "visited": false
+}]
 
 // Select query for projects 
 app.get('/',(req,resp)=>{
-    client.query("SELECT * FROM locations",
-    (err, rows,fields)=> {
-    if(err){
-        res.send(error)
-    }else{
-        resp.json(rows.rows)
-    }
-})
+    res.send(locations)
 })
 
 // BodyParser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-port = process.env.PORT
+port = 5432 
 // Listener
 app.listen(port, ()=>{
 console.log('Im listening on ', port)
